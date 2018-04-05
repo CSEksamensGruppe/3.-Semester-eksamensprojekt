f02759a9d37296490dfcd01893cf385bcfd37502
DunkRing <Chris31@live.dk>
2018-04-05 12:03:07 +0200

    Testmiljø
    Console app med testning af miljø

---
+++ TestOpsætningMiljø/TestOpsætningMiljø/Udregning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOpsætningMiljø
+{
+    public class Udregning
+    {
+        public double Division(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ TestOpsætningMiljø/TestOpsætningMiljøTests/UdregningTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestOpsætningMiljø;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOpsætningMiljø.Tests
+{
+    [TestClass()]
+    public class UdregningTests
+    {
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Udregning udregning = new Udregning();
+
+            double actualvalue = udregning.Division(10, 2);
+            double expectedvalue = 5;
+
+
+            Assert.AreEqual(expectedvalue, actualvalue);
+        }
+    }
+}
